c42d4c212a47441535bf8e55589fa845a11880da
labuenafe <labuenafe21@gmail.com>
2024-07-03 14:55:47 -0500

    chore: Update App.js with new status text and style

--- App.js
+++ App.js
@@ -4,8 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.status}>Hola Caracola</Text>
+      <StatusBar style='dark'/>
     </View>
   );
 }
@@ -13,8 +13,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#2bf',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  status:{
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: 'bold',
+  }
 });
