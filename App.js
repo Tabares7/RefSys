3c750674ffaa5fd52e07967cd60f7d3e56018c73
labuenafe <labuenafe21@gmail.com>
2024-07-03 16:10:54 -0500

    refactor: Remove unused imports and update dependencies in App.js

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { NativeRouter } from 'react-router-native';
 import { Main } from './components/Main';
 
 export default function App() {
   return (
     <NativeRouter>
-
     <Main />
     </NativeRouter>
   );
